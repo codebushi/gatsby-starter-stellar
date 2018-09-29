324b1d5baa1bd5add1cb0c20317bb0d27766248b
Hunter Chang <hunter@madwiremedia.com>
2018-09-29 12:44:48 -0600

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Gatsby Starter - Stellar by HTML5 UP",
+    author: "Hunter Chang",
+    description: "A Gatsby.js Starter based on Stellar by HTML5 UP"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
