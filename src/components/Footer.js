7ebac806cd4f328d2b3a195e622a07949a8e0c48
Takio Yamaoka <y.takio@gmail.com>
2021-04-10 20:58:25 +0900

    update some link attributes to open on other tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,42 +7,36 @@ const Footer = props => (
       <h2>ご質問やお問合せなどはこちら</h2>
       <ul className="actions">
         <li>
-          <a href="https://discord.gg/Txh3GjCJYk" className="button">
+          <a href="https://discord.gg/Txh3GjCJYk" target="_blank" rel="noopener noreferrer" className="button">
             Discord
           </a>
         </li>
       </ul>
       <ul className="icons">
         <li>
-          <a
-            href="https://twitter.com/"
-            className="icon fa-twitter alt"
-          >
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className="icon fa-twitter alt" >
             <span className="label">Twitter</span>
           </a>
         </li>
         <li>
-          <a href="https://discord.gg/Txh3GjCJYk" className="icon fa-comment alt">
+          <a href="https://discord.gg/Txh3GjCJYk" target="_blank" rel="noopener noreferrer" className="icon fa-comment alt">
             <span className="label">Discord</span>
           </a>
         </li>
         <li>
-          <a
-            href="https://github.com/coderdojo-higashi-sumiyoshi"
-            className="icon fa-github alt"
-          >
+          <a href="https://github.com/coderdojo-higashi-sumiyoshi" target="_blank" rel="noopener noreferrer" className="icon fa-github alt">
             <span className="label">GitHub</span>
           </a>
         </li>
         <li>
-          <a href="" className="icon fa-envelope alt">
+          <a href="" target="_blank" rel="noopener noreferrer" className="icon fa-envelope alt">
             <span className="label">Mail</span>
           </a>
         </li>
       </ul>
     </section>
     <p className="copyright">
-      &copy; Untitled. Design: <a href="https://html5up.net">HTML5 UP</a>.<br/>
+      &copy; Untitled. Design: <a href="https://html5up.net" target="_blank" rel="noopener noreferrer">HTML5 UP</a>.<br/>
       &copy; CoderDojo Osakasayama / Hommachi.<br/>
       &copy; CoderDojo Higashi-Sumiyoshi.<br/>
     </p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -42,7 +42,7 @@ class Index extends React.Component {
                 <header className="major">
                   <h2>コーダー道場って？</h2>
                   <p>
-                    CoderDojoは 7 ~ 17 才をの子どもたちの楽しくプログラミングをする道場です。<br/>
+                    CoderDojoは 7 ~ 17 才の子どもたちが楽しくプログラミングできる道場です。<br/>
                     世界に2,200の道場、日本には228以上の道場があります。<br/>
                   </p>
                   <p>
@@ -51,7 +51,7 @@ class Index extends React.Component {
                 </header>
                 <ul className="actions">
                   <li>
-                    <a href="https://coderdojo.jp/" className="button special">
+                    <a href="https://coderdojo.jp/" target="_blank" rel="noopener noreferrer" className="button special">
                       もっとくわしく
                     </a>
                   </li>
@@ -70,7 +70,7 @@ class Index extends React.Component {
             <ul className="features">
               <li>
                 <span className="icon major style1 fa-gamepad"></span>
-                <h3><a href="https://scratch.mit.edu/">スクラッチ</a></h3>
+                <h3><a href="https://scratch.mit.edu/" target="_blank" rel="noopener noreferrer">スクラッチ</a></h3>
                 <p>
                   ゲームやアニメーションを作ろう
                 </p>
@@ -84,7 +84,7 @@ class Index extends React.Component {
               </li>
               <li>
                 <span className="icon major style5 fa-microchip"></span>
-                <h3><a href="https://microbit.org/">micro:bit</a></h3>
+                <h3><a href="https://microbit.org/" target="_blank" rel="noopener noreferrer">micro:bit</a></h3>
                 <p>
                   コンピュータボードで電子工作
                 </p>
@@ -96,20 +96,26 @@ class Index extends React.Component {
             <header className="major">
               <h2>コーダー道場に参加するには</h2>
               <p>
-                7 歳〜 17 歳までのプログラミング好きな子ども達が参加できます。<br/>
-                子ども達といっしょに楽しみたい大人もメンターとして参加できます。
+                プログラミングを好きな 7 歳〜 17 歳までの子どもが参加できます。<br/>
+                子ども達といっしょに楽しみたい大人はメンターとして参加できます。
               </p>
             </header>
             <p>
-              不定期開催 (2ヶ月に1回)<br/>
-              当面はオンライン開催の予定
+              不定期開催 (2ヶ月に1回くらい)<br/>
+              開催場所は東田辺周辺を予定してます<br/>
+              ただし当面はオンライン開催(Discord)の予定
             </p>
             <footer className="major">
               <ul className="actions">
                 <li>
-                  <Link to="/generic" className="button special">
+                  <a href="https://discord.gg/Txh3GjCJYk" target="_blank" rel="noopener noreferrer" className="button special">
                     さんかページへ(準備中)
-                  </Link>
+                  </a>
+                </li>
+                <li>
+                  <a href="https://discord.gg/Txh3GjCJYk" target="_blank" rel="noopener noreferrer" className="button">
+                    Discord
+                  </a>
                 </li>
               </ul>
             </footer>
@@ -125,13 +131,8 @@ class Index extends React.Component {
             <footer className="major">
               <ul className="actions">
                 <li>
-                  <Link to="/generic" className="button special">
-                    Get Started
-                  </Link>
-                </li>
-                <li>
                   <Link to="/generic" className="button">
-                    Learn More
+                    ブログ
                   </Link>
                 </li>
               </ul>
