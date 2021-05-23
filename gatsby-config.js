1e618e71a1bf693e567c80bf300d18661e7062bc
Takio Yamaoka <y.takio@gmail.com>
2021-05-23 09:49:38 +0900

    disable `plugin-offline`

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
+//    'gatsby-plugin-offline',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
